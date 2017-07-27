d407c545919c0f4453c14ad9be137590cccbe1e5
sracca <30498954+sracca@users.noreply.github.com>
2017-07-27 11:13:06 -0400

    Add files via upload

---
+++ phpsqlsearch_dbinfo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Providence Mutual Agent Locator
+ * Database Info/Credentials
+ *Author: Stephanie Racca
+ * Date Created: 7/27/17
+ * Last Update: //
+ */
+
+//Add this in later
+$username="username";
+$password="password";
+$database="username=databaseName";
+?>
